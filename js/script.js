9b325e77e3c0f18ceb2b4d0c64f60a0956e76fca
everyline94 <brunolujanlarocca@gmail.com>
2024-08-27 16:37:47 -0300

    início da pagina sobre

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@ menuBtn.onclick = () => {
 }
 
 var swiper = new Swiper(".home-slider", { 
+    grabCursor: true,
+    loop: true,
+    centeredSlides:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
-    },
-    loop: true 
+    }
   });

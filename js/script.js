cce0d1d5cda5a04e4c6acbc1bbc271d1bb10b838
everyline94 <brunolujanlarocca@gmail.com>
2024-08-27 15:30:41 -0300

    modificações no css, adição do swiper e criação do arquivo .js do projeto

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn')
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".home-slider", { 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true 
+  });
